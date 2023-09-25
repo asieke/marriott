d66f0b5d32c44d461d3fab92bf825474bf4dd983
Alex Sieke <asieke@gmail.com>
2023-09-25 10:04:12 -0400

    initial scaffolding

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="grid grid-cols-2 gap-4 p-4">
+	<div class="component">Component 1</div>
+	<div class="component">Component 1</div>
+</div>
+
+<style>
+	.component {
+		@apply shadow-xl rounded-lg p-5 bg-white;
+	}
+</style>

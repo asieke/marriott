1cb7a7b571bceb5a78f2044980072f04f6320db3
Alex Sieke <asieke@gmail.com>
2023-09-26 12:48:36 -0400

    restored components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,14 +22,14 @@
 	// console.log(data);
 </script>
 
-<button class="w-32 h-16 bg-white" on:click={login}>Log Data</button>
+<button class="w-32 h-16 -mt-8 bg-white" on:click={login}>Log Data</button>
 <div class="grid grid-cols-2 gap-4 p-4">
 	<div class="flex flex-col gap-4">
 		<div class="component"><DateTime /></div>
-		<!-- <div class="component"><DailyWeather {dailyWeather} /></div>
+		<div class="component"><DailyWeather {dailyWeather} /></div>
 		<div class="component"><Masterpiece {masterpiece} /></div>
 		<div class="component"><WordOfTheDay {word} /></div>
-		<div class="component"><MathProblems /></div> -->
+		<div class="component"><MathProblems /></div>
 	</div>
 	<div>
 		<div class="component">Component 1</div>

612ade2eb78992109f8224c902f53e293e49a87e
Alex Sieke <asieke@gmail.com>
2023-09-26 12:47:05 -0400

    pushing auth changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,29 @@
 <script>
 	import '../styles/app.css';
-
+	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
 
 	let random = 1;
 
 	onMount(() => {
 		//pick a random number 1-12
 		random = Math.floor(Math.random() * 12) + 1;
+
+		//initialize supabase session
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => subscription.unsubscribe();
 	});
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,19 +9,27 @@
 	import MathProblems from './components/MathProblems.svelte';
 
 	export let data;
+	const { dailyWeather, masterpiece, word, supabase } = data;
 
-	const { dailyWeather, masterpiece, word } = data;
+	console.log(data);
+
+	const login = async () => {
+		supabase.auth.signInWithOAuth({
+			provider: 'google'
+		});
+	};
 
 	// console.log(data);
 </script>
 
+<button class="w-32 h-16 bg-white" on:click={login}>Log Data</button>
 <div class="grid grid-cols-2 gap-4 p-4">
 	<div class="flex flex-col gap-4">
 		<div class="component"><DateTime /></div>
-		<div class="component"><DailyWeather {dailyWeather} /></div>
+		<!-- <div class="component"><DailyWeather {dailyWeather} /></div>
 		<div class="component"><Masterpiece {masterpiece} /></div>
 		<div class="component"><WordOfTheDay {word} /></div>
-		<div class="component"><MathProblems /></div>
+		<div class="component"><MathProblems /></div> -->
 	</div>
 	<div>
 		<div class="component">Component 1</div>
